a5e1a13728bbbc6a9af42511e73885803074c857
Dawson Toth <dawson@tothsolutions.com>
2018-08-24 23:13:52 -0400

    Adjusting Calculations

--- speed.js
+++ speed.js
@@ -1,4 +1,6 @@
-let fs = require('fs');
+let fs = require('fs'),
+	rpio = require('rpio'),
+	findHighs = require('./lib/findHighs');
 
 (function() {
 	/*
@@ -6,14 +8,19 @@ let fs = require('fs');
 	 */
 
 	let INPUT_PIN = 7;
-	let BUFFER_LENGTH = 1e7; // 1e8 goes for about 14 seconds, 100mb... too big?
-	let NANOSECONDS_IN_A_SECOND = 1e9;
+	let BUFFER_LENGTH = 2e7;
+	let MEASUREMENT_INTERVAL = 5000;
 	let WHEEL_DIAMETER_IN_INCHES = 4.5;
+	let WHEEL_RATIO = 27.896592409566857 / 6;
+	let AVERAGE_SIZE = 3;
+
+	let NANOSECONDS_IN_A_SECOND = 1e9;
 	let INCHES_IN_A_MILE = 63360;
 	let WHEEL_CIRCUMFERENCE_IN_MILES = Math.PI * WHEEL_DIAMETER_IN_INCHES / INCHES_IN_A_MILE;
-	let WHEEL_RATIO = 27.896592409566857 / 6;
-	// TODO: Tweak the measurement interval.
-	let MEASUREMENT_INTERVAL = 3000;
+
+	let lastSpeeds = [];
+	let lastCadences = [];
+	// let fileCounter = 0;
 
 	runOnPi();
 
@@ -24,14 +31,23 @@ let fs = require('fs');
 		/*
 		 Initialization.
 		 */
-		let rpio = require('rpio');
 		let buffer = new Buffer(BUFFER_LENGTH);
 		rpio.open(INPUT_PIN, rpio.INPUT);
 
 		/*
 		 Sample.
 		 */
-		// let fileCounter = 0;
+
+
+		function runSimulation() {
+			let buffer = fs.readFileSync('./buffer-4-14363869422.data'),
+				totalNanoseconds = 14363869422 / 10,
+				parsed = convertToCounters(buffer);
+			for (let i = 0; i < 10; i++) {
+				calculateAndWriteResults(totalNanoseconds, parsed);
+			}
+		}
+
 		setInterval(
 			() => {
 
@@ -41,19 +57,11 @@ let fs = require('fs');
 				let elapsed = process.hrtime(start),
 					totalNanoseconds = elapsed[0] * NANOSECONDS_IN_A_SECOND + elapsed[1];
 
-				// TODO: Get a measurement of this, with me on the belt. Run for a minute and count cadence.
 				// fs.writeFileSync('./buffer-' + (++fileCounter) + '-' + totalNanoseconds + '.data', buffer);
-				logAnalysis(totalNanoseconds, convertToCounters(buffer));
+				calculateAndWriteResults(totalNanoseconds, convertToCounters(buffer));
 			},
 			MEASUREMENT_INTERVAL
 		);
-	}
-
-	function runSimulation() {
-		let buffer = fs.readFileSync('../buffer-4-14363869422.data'),
-			totalNanoseconds = 14363869422 / 10,
-			parsed = convertToCounters(buffer);
-		logAnalysis(totalNanoseconds, parsed);
 	}
 
 	function convertToCounters(buffer) {
@@ -72,9 +80,13 @@ let fs = require('fs');
 		return counters;
 	}
 
-	function logAnalysis(totalNanoseconds, counters) {
-		let firstChangedToOnIndex = counters.findIndex((count, index) => index > 0 && count > 0),
-			lastChangedToOffIndex = counters.lastIndexOf(counters.slice().reverse().find((count, index) => index > 0 && count < 0)),
+	function calculateAndWriteResults(totalNanoseconds, counters) {
+		let onOutliers = calculateOutliers(counters.filter(c => c > 0)),
+			offOutliers = calculateOutliers(counters.filter(c => c < 0)),
+			firstChangedToOnIndex = counters.findIndex((count, index) => index > 0 && count > onOutliers.min && count < onOutliers.max),
+			lastChangedToOffIndex = counters.lastIndexOf(counters.slice().reverse().find((count, index) => index > 0 && count > offOutliers.min && count < offOutliers.max)),
+			// firstChangedToOnIndex = counters.findIndex((count, index) => index > 0 && count > 0),
+			// lastChangedToOffIndex = counters.lastIndexOf(counters.slice().reverse().find((count, index) => index > 0 && count > 0)),
 			usableCounters = counters.slice(firstChangedToOnIndex, lastChangedToOffIndex),
 			usableIterations = counters.reduce((total, current) => total + current, 0),
 			magnetCounter = usableCounters.filter(c => c > 0).length,
@@ -87,7 +99,20 @@ let fs = require('fs');
 			rotationsPerMinute = rotationsPerSecond * 60,
 			rotationsPerHour = rotationsPerMinute * 60,
 			wheelMilesPerHour = rotationsPerHour * WHEEL_CIRCUMFERENCE_IN_MILES,
-			beltMilesPerHour = wheelMilesPerHour / WHEEL_RATIO;
+			beltMilesPerHour = wheelMilesPerHour / WHEEL_RATIO,
+			highs = findHighs(counters),
+			cadence = highs
+				? cleanCadence(highs / usableTotalSeconds * 60)
+				: 0;
+
+		lastCadences.push(cadence);
+		if (lastCadences.length > AVERAGE_SIZE) {
+			lastCadences.shift();
+		}
+		lastSpeeds.push(beltMilesPerHour);
+		if (lastSpeeds.length > AVERAGE_SIZE) {
+			lastSpeeds.shift();
+		}
 		console.log('~~~~~~~~~~~');
 		console.log('Trimmed Buffer %:', usableBufferPercentage * 100);
 		console.log('Trimmed Seconds:', usableTotalSeconds);
@@ -95,16 +120,54 @@ let fs = require('fs');
 		console.log('RPM:', rotationsPerMinute);
 		console.log('Wheel:', wheelMilesPerHour + ' MPH');
 		console.log('Belt:', beltMilesPerHour + ' MPH');
-		// TODO: Apply some smoothing over a short period of time? Like a 10 second average, perhaps.
-		fs.writeFileSync('./currentSpeed.txt', beltMilesPerHour, 'UTF-8');
-		// TODO: Calculate cadence based on fluctuations in the readings.
-		fs.writeFileSync(
-			'./currentCadence.txt',
-			beltMilesPerHour > 0.5
-				? beltMilesPerHour >= 5
-				? 172
-				: 100
-				: 0,
-			'UTF-8');
+		console.log('Avg Belt:', avg(lastSpeeds) + ' MPH');
+		console.log('Cadence:', cadence);
+		console.log('Avg Cadence:', avg(lastCadences));
+
+		// if (--waitFor <= 0 && --measureFor >= 0) {
+		// 	fs.writeFileSync('./' + measurePrefix + '-' + (measureCounter++) + '.json', JSON.stringify(counters), 'UTF-8');
+		// 	console.log('measuring ' + measureCounter);
+		// }
+		fs.writeFileSync('./currentSpeed.txt', avg(lastSpeeds), 'UTF-8');
+		fs.writeFileSync('./currentCadence.txt', avg(lastCadences), 'UTF-8');
 	}
 })();
+
+function calculateOutliers(someArray) {
+	if (someArray.length < 4) {
+		return someArray;
+	}
+
+	let values, q1, q3, iqr, maxValue, minValue;
+
+	values = someArray.slice().sort((a, b) => a - b);//copy array fast and sort
+
+	if ((values.length / 4) % 1 === 0) {//find quartiles
+		q1 = 1 / 2 * (values[(values.length / 4)] + values[(values.length / 4) + 1]);
+		q3 = 1 / 2 * (values[(values.length * (3 / 4))] + values[(values.length * (3 / 4)) + 1]);
+	} else {
+		q1 = values[Math.floor(values.length / 4 + 1)];
+		q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
+	}
+
+	iqr = q3 - q1;
+	maxValue = q3 + iqr * 1.5;
+	minValue = q1 - iqr * 1.5;
+
+	// return someArray.filter(x => (x >= minValue) && (x <= maxValue));
+	return { min: minValue, max: maxValue };
+}
+
+function avg(v) {
+	return v.reduce((a, b) => a + b, 0) / v.length;
+}
+
+function cleanCadence(val) {
+	if (val < 90) {
+		return 0;
+	}
+	if (val > 220) {
+		return 0;
+	}
+	return Math.round(val);
+}
