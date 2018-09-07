0c284094707fea4f3a703b5b01874f110dfa3d92
Dawson Toth <dawson@tothsolutions.com>
2018-09-07 02:13:39 -0400

    Clean up

--- speed.js
+++ speed.js
@@ -1,32 +1,37 @@
-let fs = require('fs'),
-	GPIO = require('onoff').Gpio,
+const
+	fs = require('fs'),
 	findHighs = require('./lib/findHighs'),
-	outliers = require('./lib/outliers');
+	outliers = require('./lib/outliers'),
+	GPIO = require('onoff').Gpio;
 
 /*
  Constants.
  */
-let NANOSECONDS_IN_A_SECOND = 1e9;
-let MILLISECONDS_IN_A_SECOND = 1e3;
-let INCHES_IN_A_MILE = 63360;
+const NANOSECONDS_IN_A_SECOND = 1e9;
+const MILLISECONDS_IN_A_SECOND = 1e3;
+const INCHES_IN_A_MILE = 63360;
 
 /*
  Configuration.
  */
-
-let USE_HR_TIME = true;
-let INPUT_PIN = 4;
-let MINIMUM_SPEED = 0.8;
-let UPDATE_EVERY_MILLISECONDS = 1000;
-let SPEED_SAMPLE_PERIOD = 5 * (USE_HR_TIME ? NANOSECONDS_IN_A_SECOND : MILLISECONDS_IN_A_SECOND);
-let CADENCE_SAMPLE_PERIOD = 10 * (USE_HR_TIME ? NANOSECONDS_IN_A_SECOND : MILLISECONDS_IN_A_SECOND);
-let WHEEL_DIAMETER_IN_INCHES = 4.5;
-let WHEEL_RATIO = 18.139186148152856 / 4.0;
-let MAGNETS_ON_THE_WHEEL = 1;
+const INPUT_PIN = 4;
+const MIN_SPEED = 0.8;
+const MAX_SPEED = 16;
+const MIN_CADENCE = 90;
+const MAX_CADENCE = 230;
+const USE_HR_TIME = true;
+const UPDATE_EVERY_MILLISECONDS = 1000;
+const SPEED_SAMPLE_PERIOD = 5 * (USE_HR_TIME ? NANOSECONDS_IN_A_SECOND : MILLISECONDS_IN_A_SECOND);
+const CADENCE_SAMPLE_PERIOD = 12 * (USE_HR_TIME ? NANOSECONDS_IN_A_SECOND : MILLISECONDS_IN_A_SECOND);
+const WHEEL_DIAMETER_IN_INCHES = 4.5;
+const WHEEL_RATIO = 18.139186148152856 / 4.0;
+const MAGNETS_ON_THE_WHEEL = 2;
+const HIGHS_TOLERANCE_PERCENT = 0.05;
 
 /*
  Debugging.
  */
+let writeDebugLines = true;
 let waitFor = 10;
 let measureFor = 0;
 let measureCounter = 0;
@@ -34,21 +39,27 @@ let measureCounter = 0;
 /*
  State.
  */
-let magnet = new GPIO(INPUT_PIN, 'in', 'rising');
+let magnet = new GPIO(INPUT_PIN, 'in', 'falling');
 let lastPassedAt = USE_HR_TIME
 	? process.hrtime()
 	: Date.now();
 let passes = [];
-let maxSample = Math.max(SPEED_SAMPLE_PERIOD, CADENCE_SAMPLE_PERIOD);
-let WHEEL_CIRCUMFERENCE_IN_MILES = Math.PI * WHEEL_DIAMETER_IN_INCHES / INCHES_IN_A_MILE;
+let maxSampleTime = Math.max(SPEED_SAMPLE_PERIOD, CADENCE_SAMPLE_PERIOD);
+let wheelCircumferenceInMiles = Math.PI * WHEEL_DIAMETER_IN_INCHES / INCHES_IN_A_MILE;
 
 /*
  Initialization.
  */
-
 updateCalculations();
 setInterval(updateCalculations, UPDATE_EVERY_MILLISECONDS);
-magnet.watch((err, value) => {
+magnet.watch(magnetPassed);
+require('death')(cleanUp);
+
+/*
+ Implementation.
+ */
+
+function magnetPassed(err) {
 	if (err) {
 		console.error(err);
 		// TODO: Do something with the error.
@@ -61,55 +72,41 @@ magnet.watch((err, value) => {
 			? process.hrtime()
 			: Date.now();
 	}
-});
-
-require('death')(() => {
-	try {
-		magnet.unexport();
-	}
-	catch (err) {
-		// Oh well!
-	}
-	process.exit(0);
-});
-
-/*
- Utility methods.
- */
+}
 
 function updateCalculations() {
-	let currentPass = USE_HR_TIME
+	let timeSinceLastPass = USE_HR_TIME
 		? convertElapsedToNanoseconds(process.hrtime(lastPassedAt))
-		: Date.now() - lastPassedAt,
-		totalTime = currentPass,
+		: (Date.now() - lastPassedAt),
+		sampledTime = 0,
 		trimTo = -1,
 		magnetPassesForSpeed = 0,
 		speedCalculated = false,
 		cadenceCalculated = false;
 
-	if (passes.length === 0) {
-		calculateAndWriteSpeed(totalTime, 0);
-		calculateAndWriteCadence(totalTime, []);
+	if (passes.length === 0 || timeSinceLastPass > maxSampleTime) {
+		calculateAndWriteSpeed(sampledTime, 0);
+		calculateAndWriteCadence(sampledTime, []);
 		return;
 	}
 	for (let i = 0; i < passes.length; i++) {
-		totalTime += passes[i];
+		sampledTime += passes[i];
 		if (!speedCalculated) {
-			if (totalTime > SPEED_SAMPLE_PERIOD) {
+			if (sampledTime > SPEED_SAMPLE_PERIOD) {
 				speedCalculated = true;
-				calculateAndWriteSpeed(totalTime, magnetPassesForSpeed);
+				calculateAndWriteSpeed(sampledTime, magnetPassesForSpeed);
 			}
 			else {
 				magnetPassesForSpeed += 1;
 			}
 		}
 		if (!cadenceCalculated) {
-			if (totalTime > CADENCE_SAMPLE_PERIOD) {
+			if (sampledTime > CADENCE_SAMPLE_PERIOD) {
 				cadenceCalculated = true;
-				calculateAndWriteCadence(totalTime, passes.slice(0, trimTo));
+				calculateAndWriteCadence(sampledTime, passes.slice(0, trimTo));
 			}
 		}
-		if (totalTime > maxSample) {
+		if (sampledTime > maxSampleTime) {
 			trimTo = i;
 			break;
 		}
@@ -130,36 +127,51 @@ function calculateAndWriteSpeed(elapsedTime, magnetCounter) {
 		rotationsPerSecond = fullRotations / totalSeconds,
 		rotationsPerMinute = rotationsPerSecond * 60,
 		rotationsPerHour = rotationsPerMinute * 60,
-		wheelMilesPerHour = rotationsPerHour * WHEEL_CIRCUMFERENCE_IN_MILES,
+		wheelMilesPerHour = rotationsPerHour * wheelCircumferenceInMiles,
 		beltMilesPerHour = wheelMilesPerHour / WHEEL_RATIO
 	;
 
-	if (beltMilesPerHour < MINIMUM_SPEED) {
+	if (beltMilesPerHour < MIN_SPEED || beltMilesPerHour > MAX_SPEED) {
 		beltMilesPerHour = 0;
 	}
-
-	console.log('~~~~~~~~~~~');
-	console.log('Magnet Count:', magnetCounter);
-	console.log('RPM:', rotationsPerMinute);
-	console.log('Wheel:', wheelMilesPerHour + ' MPH');
-	console.log('Belt:', beltMilesPerHour + ' MPH');
 	fs.writeFileSync('./currentSpeed.txt', beltMilesPerHour, 'UTF-8');
+
+	if (writeDebugLines) {
+		console.log('~~~~~~~~~~~');
+		console.log('Magnet Count:', magnetCounter);
+		console.log('RPM:', rotationsPerMinute);
+		console.log('Wheel:', wheelMilesPerHour + ' MPH');
+		console.log('Belt:', beltMilesPerHour + ' MPH');
+	}
 }
 
 function calculateAndWriteCadence(elapsedTime, passes) {
-	let highs = passes.length && findHighs(passes),
+	let highs = passes.length && findHighs(passes, HIGHS_TOLERANCE_PERCENT),
 		rawCadence = highs
 			? highs / (elapsedTime / (USE_HR_TIME ? NANOSECONDS_IN_A_SECOND : MILLISECONDS_IN_A_SECOND)) * 60
 			: 0,
-		cadence = rawCadence < 90 || rawCadence > 230 ? 0 : Math.round(rawCadence);
+		cadence = rawCadence < MIN_CADENCE || rawCadence > MAX_CADENCE
+			? 0
+			: Math.round(rawCadence);
+	fs.writeFileSync('./currentCadence.txt', cadence, 'UTF-8');
 
-	console.log('~~~~~~~~~~~');
-	console.log('Raw Cadence:', rawCadence);
-	console.log('Cadence:', cadence);
-
+	if (writeDebugLines) {
+		console.log('~~~~~~~~~~~');
+		console.log('Raw Cadence:', rawCadence);
+		console.log('Cadence:', cadence);
+	}
 	if (measureFor >= 0 && --waitFor <= 0 && --measureFor >= 0) {
 		fs.writeFileSync('./data/cadence-passes-' + (measureCounter++) + '.json', JSON.stringify(outliers.filter(passes)), 'UTF-8');
 		console.log('measuring ' + measureCounter);
 	}
-	fs.writeFileSync('./currentCadence.txt', cadence, 'UTF-8');
+}
+
+function cleanUp() {
+	try {
+		magnet.unexport();
+	}
+	catch (err) {
+		// Oh well!
+	}
+	process.exit(0);
 }
