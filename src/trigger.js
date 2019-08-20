bc74fd717385cb981dc1bb846faf2aeef0cf8c7f
Dawson Toth <dawson@tothsolutions.com>
2019-08-19 23:34:58 -0400

    Add pass dumping for diagnostics

--- src/trigger.js
+++ src/trigger.js
@@ -10,7 +10,10 @@ let fs = require('fs'),
 let trigger = new GPIO(constants.INPUT_PIN, 'in', 'rising'),
 	passHistory = [],
 	lastPassedAt = 0,
-	passesAveraged = 0;
+	passesAveraged = 0,
+	rawHistoryForDump = [],
+	shouldDump = false,
+	ignoreFirstDump = true;
 
 /*
  Initialization.
@@ -18,7 +21,7 @@ let trigger = new GPIO(constants.INPUT_PIN, 'in', 'rising'),
 ipc.boot(ipc.keys.trigger);
 trigger.watch(triggerPassed);
 require('death')(cleanUp);
-// setTimeout(dumpPasses, 12e3);
+shouldDump && setInterval(dumpPasses, 60 * 1000);
 
 /*
  Implementation.
@@ -37,6 +40,7 @@ function triggerPassed(err) {
 		let elapsedTime = constants.USE_HR_TIME
 			? utils.convertElapsedToNanoseconds(process.hrtime(lastPassedAt))
 			: Date.now() - lastPassedAt;
+		shouldDump && rawHistoryForDump.push(elapsedTime);
 		if (passHistory.length > constants.PASS_AVERAGE_PERIOD) {
 			let recentPassesAverage = passHistory.slice(0, constants.PASS_AVERAGE_PERIOD).reduce((a, b) => a + b, 0) / constants.PASS_AVERAGE_PERIOD,
 				deltaAverage = Math.abs(recentPassesAverage - elapsedTime),
@@ -58,15 +62,16 @@ function triggerPassed(err) {
 }
 
 function dumpPasses() {
-	let copyPasses = passHistory.slice();
-	fs.writeFile('../data/raw-passes-1.json', JSON.stringify(copyPasses), 'UTF-8', err => {
+	if (ignoreFirstDump) {
+		ignoreFirstDump = false;
+		return;
+	}
+	fs.writeFile('../dump/' + new Date().toLocaleString() + '.json', JSON.stringify(rawHistoryForDump), 'UTF-8', err => {
 		if (err) {
 			console.error(err);
 		}
-		else {
-			console.log('Passes written to ./data/raw-passes-1.json');
-		}
 	});
+	rawHistoryForDump = [];
 }
 
 function cleanUp() {
